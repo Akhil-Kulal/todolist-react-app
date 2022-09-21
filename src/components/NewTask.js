f548682a9d8d4a008a6dc212dec0778e289a0368
Akhil-Kulal <akhilkulal9@gmail.com>
2022-09-21 20:29:26 +0530

    Add style to NewTask component

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 
 class NewTask extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="newTask">
+        <div className="heading">
+          <h1>Todolist App</h1>
+        </div>
+        <div className="newCard">
+          <input
+            type="text"
+            className="newInput"
+            placeholder="Add New Task..."
+          />
+          <button className="addButton">Add</button>
+        </div>
+      </div>
+    );
   }
 }
 
