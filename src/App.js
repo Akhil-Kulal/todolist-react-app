3c52b984abd516f5abc4be78c0224dbbce3ee714
Akhil-Kulal <akhilkulal9@gmail.com>
2022-09-22 10:14:19 +0530

    Fetch all todos from API and store the response in the array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useEffect, useState } from "react";
 import "./assets/App.css";
 import NewTask from "./components/NewTask";
 
 function App() {
+  const [items, setitems] = useState([]);
+  const [DataisLoaded, setDataisLoaded] = useState(false);
+
+  // fetches the tasks list from API as soon as the window is loaded and stores the response in an array
+  useEffect(() => {
+    fetch(" https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then(
+        (json) => setitems(json),
+        setDataisLoaded(true),
+        console.log(items)
+      );
+  }, []);
+
   return (
     <div className="App">
       <NewTask />
